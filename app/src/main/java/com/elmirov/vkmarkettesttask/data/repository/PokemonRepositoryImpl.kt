e6f7a1165d55ccf1e8ce6a11d8a2b2ed75abb8e7
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-11 18:24:55 +0700

    Получение диспатчера через констрруктор в репозитории

--- app/src/main/java/com/elmirov/vkmarkettesttask/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/elmirov/vkmarkettesttask/data/repository/PokemonRepositoryImpl.kt
@@ -2,19 +2,21 @@ package com.elmirov.vkmarkettesttask.data.repository
 
 import com.elmirov.vkmarkettesttask.data.mapper.toEntities
 import com.elmirov.vkmarkettesttask.data.network.PokemonApi
+import com.elmirov.vkmarkettesttask.di.annotation.DispatcherIo
 import com.elmirov.vkmarkettesttask.domain.entity.PartialPokemon
 import com.elmirov.vkmarkettesttask.domain.entity.Result
 import com.elmirov.vkmarkettesttask.domain.repository.PokemonRepository
 import com.elmirov.vkmarkettesttask.util.getResultWithHandleError
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class PokemonRepositoryImpl @Inject constructor(
     private val api: PokemonApi,
+    @DispatcherIo private val dispatcherIo: CoroutineDispatcher,
 ) : PokemonRepository {
 
     override suspend fun getAll(): Result<List<PartialPokemon>> = getResultWithHandleError(
-        dispatcher = Dispatchers.IO,
+        dispatcher = dispatcherIo,
         data = {
             api.getAll().toEntities()
         }
