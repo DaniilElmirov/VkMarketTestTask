29b6cc7a6c422643dc8fc677ef658bb61637d48e
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-11 20:07:16 +0700

    Добавил базовую реализацию экрана со списком покемонов

---
+++ app/src/main/java/com/elmirov/vkmarkettesttask/ui/list/PokemonsFragment.kt
@@ -0,0 +1,24 @@
+package com.elmirov.vkmarkettesttask.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.elmirov.vkmarkettesttask.databinding.FragmentPokemonsBinding
+
+class PokemonsFragment: Fragment() {
+
+    private var _binding: FragmentPokemonsBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentPokemonsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
