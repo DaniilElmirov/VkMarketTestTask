b09467479cfd8c5212121c59a9ec4a64ea3f6c10
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-12 13:25:54 +0700

    Добавил метод в api для получения детальной информации

--- app/src/main/java/com/elmirov/vkmarkettesttask/data/network/PokemonApi.kt
+++ app/src/main/java/com/elmirov/vkmarkettesttask/data/network/PokemonApi.kt
@@ -1,10 +1,15 @@
 package com.elmirov.vkmarkettesttask.data.network
 
 import com.elmirov.vkmarkettesttask.data.model.AllPokemonsResponse
+import com.elmirov.vkmarkettesttask.data.model.FullPokemonResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokemonApi {
 
-    @GET("pokemon?limit=10&offset=0")
+    @GET("pokemon?limit=40&offset=0")
     suspend fun getAll(): AllPokemonsResponse
+
+    @GET("pokemon/{name}/")
+    suspend fun getByName(@Path("name") name: String): FullPokemonResponse
 }
