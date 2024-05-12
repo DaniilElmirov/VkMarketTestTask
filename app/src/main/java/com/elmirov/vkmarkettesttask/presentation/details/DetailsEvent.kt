13cf554406fcf5e1ecf859a766edf05d16a0cc31
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-12 14:03:10 +0700

    Добавил event экрана детальной информации

---
+++ app/src/main/java/com/elmirov/vkmarkettesttask/presentation/details/DetailsEvent.kt
@@ -0,0 +1,18 @@
+package com.elmirov.vkmarkettesttask.presentation.details
+
+import com.elmirov.vkmarkettesttask.domain.entity.FullPokemon
+
+sealed interface DetailsEvent {
+
+    sealed interface Ui : DetailsEvent {
+        data object Init : Ui
+
+        data object OnRefreshClick : Ui
+    }
+
+    sealed interface Internal : DetailsEvent {
+        data class DetailsLoadingSuccess(val data: FullPokemon) : Internal
+
+        data object DetailsLoadingError : Internal
+    }
+}
