9decd6f413fa6edaf6d5980a6496d3d73db51657
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-12 13:52:58 +0700

    Добавил метод маппинга для детальной информации

--- app/src/main/java/com/elmirov/vkmarkettesttask/data/mapper/PokemonMapper.kt
+++ app/src/main/java/com/elmirov/vkmarkettesttask/data/mapper/PokemonMapper.kt
@@ -1,11 +1,13 @@
 package com.elmirov.vkmarkettesttask.data.mapper
 
 import com.elmirov.vkmarkettesttask.data.model.AllPokemonsResponse
+import com.elmirov.vkmarkettesttask.data.model.FullPokemonResponse
 import com.elmirov.vkmarkettesttask.data.model.PartialPokemonDao
+import com.elmirov.vkmarkettesttask.domain.entity.FullPokemon
 import com.elmirov.vkmarkettesttask.domain.entity.PartialPokemon
 
 private const val IMAGE_URL_FORMAT =
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%s.png"
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%s"
 
 fun AllPokemonsResponse.toEntities(): List<PartialPokemon> = results.map { it.toEntity() }
 
@@ -16,10 +18,22 @@ private fun PartialPokemonDao.toEntity(): PartialPokemon {
     var imageUrl: String? = null
 
     if (id != null)
-        imageUrl = String.format(IMAGE_URL_FORMAT, id)
+        imageUrl = String.format(IMAGE_URL_FORMAT, "$id.png")
 
     return PartialPokemon(
         imageUrl = imageUrl,
         name = name,
+    )
+}
+
+fun FullPokemonResponse.toEntity(): FullPokemon {
+    val imageUrl = String.format(IMAGE_URL_FORMAT, "$id.png")
+
+    return FullPokemon(
+        imageUrl = imageUrl,
+        name = name,
+        height = height,
+        weight = weight,
+        reward = reward,
     )
 }
