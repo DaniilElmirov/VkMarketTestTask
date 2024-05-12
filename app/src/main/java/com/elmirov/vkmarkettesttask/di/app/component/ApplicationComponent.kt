db846ba8b0ce7edf1aff14df46d04e3199f9484c
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-12 14:30:01 +0700

    Добавил subcomponent для экрана детальной информации

--- app/src/main/java/com/elmirov/vkmarkettesttask/di/app/component/ApplicationComponent.kt
+++ app/src/main/java/com/elmirov/vkmarkettesttask/di/app/component/ApplicationComponent.kt
@@ -4,6 +4,7 @@ import com.elmirov.vkmarkettesttask.PokemonApplication
 import com.elmirov.vkmarkettesttask.di.app.annotation.ApplicationScope
 import com.elmirov.vkmarkettesttask.di.app.module.DataModule
 import com.elmirov.vkmarkettesttask.di.app.module.DispatcherModule
+import com.elmirov.vkmarkettesttask.di.details.component.DetailsComponent
 import com.elmirov.vkmarkettesttask.di.pokemons.component.PokemonsComponent
 import com.elmirov.vkmarkettesttask.ui.activity.MainActivity
 import dagger.Component
@@ -21,6 +22,8 @@ interface ApplicationComponent {
     fun inject(activity: MainActivity)
 
     fun pokemonsComponentFactory(): PokemonsComponent.Factory
+
+    fun detailsComponentFactory(): DetailsComponent.Factory
 
     @Component.Factory
     interface Factory {
