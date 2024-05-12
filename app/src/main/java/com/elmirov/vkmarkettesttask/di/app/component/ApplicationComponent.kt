9e2c961e06cc3a5c5f9de0d3ac59a01f288f898a
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-12 16:10:24 +0700

    Добавил navigation module в di

--- app/src/main/java/com/elmirov/vkmarkettesttask/di/app/component/ApplicationComponent.kt
+++ app/src/main/java/com/elmirov/vkmarkettesttask/di/app/component/ApplicationComponent.kt
@@ -4,6 +4,7 @@ import com.elmirov.vkmarkettesttask.PokemonApplication
 import com.elmirov.vkmarkettesttask.di.app.annotation.ApplicationScope
 import com.elmirov.vkmarkettesttask.di.app.module.DataModule
 import com.elmirov.vkmarkettesttask.di.app.module.DispatcherModule
+import com.elmirov.vkmarkettesttask.di.app.module.NavigationModule
 import com.elmirov.vkmarkettesttask.di.details.component.DetailsComponent
 import com.elmirov.vkmarkettesttask.di.pokemons.component.PokemonsComponent
 import com.elmirov.vkmarkettesttask.ui.activity.MainActivity
@@ -14,6 +15,7 @@ import dagger.Component
     modules = [
         DispatcherModule::class,
         DataModule::class,
+        NavigationModule::class,
     ]
 )
 interface ApplicationComponent {
